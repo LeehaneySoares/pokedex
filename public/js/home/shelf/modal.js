d7423475c1a21a6be0cd2a7416c6936d43529c25
Leehaney Soares <leehaney.soares@gmail.com>
2022-08-14 21:22:08 -0300

    chore(pokedex): criando arquivo para exibição do modal

---
+++ public/js/home/shelf/modal.js
@@ -0,0 +1,6 @@
+export default function () {
+  const modal = document.querySelector('.modal__area')
+  const overlayer = document.querySelector('.modal__overlayer')
+
+  console.log(modal, overlayer)
+}
