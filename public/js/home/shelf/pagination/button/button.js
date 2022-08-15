7e96c35644a79dedc38f69882c0aaaa2cc98ac6f
Leehaney Soares <leehaney.soares@gmail.com>
2022-08-14 21:19:38 -0300

    chore(pokedex.shelf): criando a paginação

--- public/js/home/shelf/pagination/button/button.js
+++ public/js/home/shelf/pagination/button/button.js
@@ -1,13 +1,15 @@
 import { $ } from '../../../shortcut.js'
 
 const buildPagination = (target) => (
-  target.inc()
+  target.nextPage()
 )
 
 export default function (target) {
   const button = document.createElement('button')
-  button.setAttribute('class', 'pokemon__button')
+  button.setAttribute('class', `pokemon__button`)
+  button.setAttribute('show', target.hasNextPage)
   button.textContent = 'Ver mais'
+
   $('.pokemon__pagination').append(button)
 
   button.addEventListener('click', () => buildPagination(target))
