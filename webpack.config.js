62807c87f3bfedd4bde90712b966873eed20d3c3
Leehaney Soares <leehaney.soares@gmail.com>
2022-10-27 18:51:32 -0300

    refactor(pokedex): alterando o output para o bundle final

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, 'public', 'assets', 'js'),
-    filename: '[name].bundle.js'
+    filename: 'bundle.js'
   },
   module: {
     rules: [
