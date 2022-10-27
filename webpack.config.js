952ebb15a9f526066dfdcecf7a957dc73dda4677
Leehaney Soares <leehaney.soares@gmail.com>
2022-10-27 18:47:32 -0300

    chore(pokedex): alterando o bundle de development para production

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, 'public', 'assets', 'js'),
